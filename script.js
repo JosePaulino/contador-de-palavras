fb4919e8097fd98c6008d9ba5ddf971bb0941a3a
José Paulino <j.paulino1@hotmail.com>
2023-05-07 12:09:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ function contPalavras() {
   var contPalavras = text.split(' ').length
 
   document.getElementById('palavras').innerHTML =
-    'Numero total de palavras: ' + contPalavras
+    'Numero total de palavras:' + contPalavras
 }
 
 function deletePalavras() {
-  var excluir = document.getElementById('').value
+  document.getElementById('area-texto').value = ''
+
+  document.getElementById('palavras').innerHTML = 'Texto Excluído '
 }
